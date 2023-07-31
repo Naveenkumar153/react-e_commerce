defc8fab64b036286aae80ae8ff358aabf6fa00c
Naveenkumar153 <naveenkumartech2025@gmail.com>
2023-07-31 21:43:20 +0530

    Register and login page design completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.scss';
 import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
 import Product from './Pages/Product';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div className="App">
       {/* <Home/> */}
       {/* <ProductList/> */}
-      <Product/>
+      {/* <Product/> */}
+      {/* <Register/> */}
+      {/* <Login/> */}
+      <Cart/>
     </div>
   );
 }
